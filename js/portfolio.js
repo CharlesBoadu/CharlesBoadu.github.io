9ba1464e1c924873b8317223b05bf32c11fc9b14
CharlesBoaduAmalitech <charles.boadu@amalitech.org>
2023-08-18 18:49:06 +0000

    Adding changes to Portfolio Page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,28 @@
-const lightbox = document.getElementById("lightbox");
-const closeButton = document.getElementById("closeButton");
-const lightboxImages = document.querySelectorAll(".lightbox-trigger");
-const largeImage = document.getElementById("largeImage");
+const smallImageOne = document.getElementById("smallImageOne");
+const smallImageTwo = document.getElementById("smallImageTwo");
 
-lightboxImages.forEach(image => {
-  image.addEventListener("click", () => {
-    largeImage.src = image.src;
-    lightbox.style.display = "block";
+const lightboxOne = document.getElementById("lightboxOne");
+const lightboxTwo = document.getElementById("lightboxTwo");
+
+const closeButtonOne = document.getElementById("closeButtonOne");
+const closeButtonTwo = document.getElementById("closeButtonTwo");
+
+smallImageOne.addEventListener("click", () => {
+    lightboxOne.style.display = "block";
     document.body.style.overflow = "hidden"; // Prevent scrolling when lightbox is open
-  });
 });
 
-closeButton.addEventListener("click", () => {
-  lightbox.style.display = "none";
-  document.body.style.overflow = "auto"; // Restore scrolling when lightbox is closed
+smallImageTwo.addEventListener("click", () => {
+    lightboxTwo.style.display = "block";
+    document.body.style.overflow = "hidden"; // Prevent scrolling when lightbox is open
+});
+
+closeButtonOne.addEventListener("click", () => {
+    lightboxOne.style.display = "none";
+    document.body.style.overflow = "auto"; // Restore scrolling when lightbox is closed
+});
+
+closeButtonTwo.addEventListener("click", () => {
+    lightboxTwo.style.display = "none";
+    document.body.style.overflow = "auto"; // Restore scrolling when lightbox is closed
 });

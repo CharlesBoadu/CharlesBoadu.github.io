4142018cbace8ad37173af2e0c6013491745aec7
CharlesBoaduAmalitech <charles.boadu@amalitech.org>
2023-08-19 01:01:32 +0000

    Adding changes to Portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,20 @@
 const smallImageOne = document.getElementById("smallImageOne");
 const smallImageTwo = document.getElementById("smallImageTwo");
+const smallImageThree = document.getElementById("smallImageThree");
+const smallImageFour = document.getElementById("smallImageFour");
+const smallImageFive = document.getElementById("smallImageFive");
 
 const lightboxOne = document.getElementById("lightboxOne");
 const lightboxTwo = document.getElementById("lightboxTwo");
+const lightboxThree = document.getElementById("lightboxThree");
+const lightboxFour = document.getElementById("lightboxFour");
+const lightboxFive = document.getElementById("lightboxFive");
 
 const closeButtonOne = document.getElementById("closeButtonOne");
 const closeButtonTwo = document.getElementById("closeButtonTwo");
+const closeButtonThree = document.getElementById("closeButtonThree");
+const closeButtonFour = document.getElementById("closeButtonFour");
+const closeButtonFive = document.getElementById("closeButtonFive");
 
 smallImageOne.addEventListener("click", () => {
     lightboxOne.style.display = "block";
@@ -14,9 +23,25 @@ smallImageOne.addEventListener("click", () => {
 
 smallImageTwo.addEventListener("click", () => {
     lightboxTwo.style.display = "block";
-    document.body.style.overflow = "hidden"; // Prevent scrolling when lightbox is open
+    document.body.style.overflow = "hidden"; 
 });
 
+smallImageThree.addEventListener("click", () => {
+    lightboxThree.style.display = "block";
+    document.body.style.overflow = "hidden"; 
+});
+
+smallImageFour.addEventListener("click", () => {
+    lightboxFour.style.display = "block";
+    document.body.style.overflow = "hidden"; 
+});
+
+smallImageFive.addEventListener("click", () => {
+    lightboxFive.style.display = "block";
+    document.body.style.overflow = "hidden"; 
+});
+
+//Close Buttons
 closeButtonOne.addEventListener("click", () => {
     lightboxOne.style.display = "none";
     document.body.style.overflow = "auto"; // Restore scrolling when lightbox is closed
@@ -24,5 +49,20 @@ closeButtonOne.addEventListener("click", () => {
 
 closeButtonTwo.addEventListener("click", () => {
     lightboxTwo.style.display = "none";
-    document.body.style.overflow = "auto"; // Restore scrolling when lightbox is closed
+    document.body.style.overflow = "auto"; 
+});
+
+closeButtonThree.addEventListener("click", () => {
+    lightboxThree.style.display = "none";
+    document.body.style.overflow = "auto"; 
+});
+
+closeButtonFour.addEventListener("click", () => {
+    lightboxFour.style.display = "none";
+    document.body.style.overflow = "auto"; 
+});
+
+closeButtonFive.addEventListener("click", () => {
+    lightboxFive.style.display = "none";
+    document.body.style.overflow = "auto"; 
 });
